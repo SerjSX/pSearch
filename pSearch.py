1e52f4895e865c346411cb4c1a9bb5c18542ce4e
Serj <74871847+SerjSX@users.noreply.github.com>
2022-08-18 16:06:58 +0300

    JSON Implementation - For Better Contribution
    sites.json has been added. The program is built to run according to the inputs in the JSON file, of course there will be special conditions depending on the site, but most will work as it is. More explained on the Github page.

--- pSearch.py
+++ pSearch.py
@@ -1,5 +1,11 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
+import json
+
+# Opens the json file which includes the supported sites and its information.
+with open('sites.json', 'r') as sites_file:
+    # Loads it, sites will be used for representation.
+    sites = json.load(sites_file)
 
 # Used as a header when requesting a website
 header= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
@@ -11,172 +17,158 @@ header= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
       'Accept-Language': 'en-US,en;q=0.8',
       'Connection': 'keep-alive'}
 
-# Website library with names
-website_library = {
-    "1": "FileCR", "2": "monkrus", "3" : "FitGirl Repacks",
-    "4": "FTUApps", "5": "VSTorrent", "6": "1337x", "7": "GOG Games",
-    "8": "STEAMRIP"
-}
-
-# Direct websites used for searching
-direct_websites = [
-    "https://filecr.com/?s=", "https://w14.monkrus.ws/search?q=",
-    "https://fitgirl-repacks.site/?s=", "https://ftuapps.dev/?s=",
-    "https://vstorrent.org/?s=", "https://1337x.to/search/", 
-    "https://gog-games.com/search/", "https://steamrip.com/?s="
-]
-
-# List and default count for best results
+# List for best results
 bestResults = list()
 
 # allLinks for appending all links at the end 
 allLinks = list()
 
-# generalMethod(website,nameInput) is the main function that searching process works in
-def generalMethod(website,nameInput):
-    bestPos = -1
-    # generalUrls is for appending the links from the direct_websites variable in order to search with those
-    # URLs only.
-    generalUrls = list()
+# generalMethod(nameInput,site_id) is the main function that searching process works in
+def generalMethod(nameInput, site_id):
+    # These variables below are for the information necessary for the searching process.
+    # siteLink has the normal link of the website, useful when the grabbed URL doesn't start with the
+    # website URL, so we can just add it easily.
+    # siteSLink has the search URL. 
+    # siteKey1-2-3 are used for grabbing the content from the website, which afterwards the links would
+    # be shown from.
+    siteLink = None
+    siteSLink = None
+    siteKey1 = None
+    siteKey2 = None
+    siteKey3 = None
 
-    # tagSoups list is where the contents get pushed in order to extract the links from them afterwards.
-    tagSoups = list()
+    # for each website in the JSON file...
+    for site in sites:
+        # If the website from the file matches the user's inserted option...
+        if site["id"] == site_id:
+            # Sets its information to the appropriate variables.
+            siteSLink = site["slink"]
+            siteLink = site["link"]
+            siteKey1 = site["skey1"]
+            siteKey2 = site["skey2"]
+            siteKey3 = site["skey3"]
 
-    # Appends the link according to the chosen website to the generalUrls list
-    try:   
-        generalUrls.append(direct_websites[int(website)-1])
-    except:
-        print("Not a valid input, quitting program.")
-        exit()
+    # resultLinks used for appending links from the results.
+    resultLinks = list()
 
-    # Starts the process of extracting and appending the links (results)
-    # For link in general Urls...
-    for url in generalUrls:
-        # generalLinks used for appending links within the generalMethod() function
-        generalLinks = list()
+    # If it starts with https://1337x.to/ then quote it with %20 (space), this is unique for this site 
+    # because of its web structure.
+    if siteLink.startswith("https://1337x.to/"):
+        nameInputFixed = urllib.parse.quote(nameInput)
+        # Connect the url with the software name the user put at the beginning
+        searchUrl = siteSLink + nameInputFixed + "/1/"
+    # If it doesn't starts with https://1337x.to/ then quote it with + (it replaces space with +), this is
+    # the default method as most sites work this way.
+    else:      
+        nameInputFixed = urllib.parse.quote_plus(nameInput)
+        # Connect the url with the software name the user put at the beginning
+        searchUrl = siteSLink + nameInputFixed
 
-        # If it starts with https://1337x.to/ then quote it with %20 (space), this is unique for this site 
-        # because of its web structure.
-        if url.startswith("https://1337x.to/"):
-            nameInputFixed = urllib.parse.quote(nameInput)
-            # Connect the url with the software name the user put at the beginning
-            searchUrl = url + nameInputFixed + "/1/"
-        # If it doesn't starts with https://1337x.to/ then quote it with + (it replaces space with +), this is
-        # the default method as most sites work this way.
-        else:      
-            nameInputFixed = urllib.parse.quote_plus(nameInput)
-            # Connect the url with the software name the user put at the beginning
-            searchUrl = url + nameInputFixed
+    # Inform the user where and what it's searching. 
+    print("\nSearching", nameInput, "at", searchUrl)
 
-        # Inform the user where and what it's searching. 
-        print("\nSearching", nameInput, "at", searchUrl)
+    # Send a request to connect to the site with the header.
+    req = urllib.request.Request(url=searchUrl, headers=header) 
 
-        # Send a request to connect to the site with the header.
-        req = urllib.request.Request(url=searchUrl, headers=header) 
-
-        # Open the URL to read
-        html = urllib.request.urlopen(req).read()
-        # Parse the page with BeautifulSoup
-        soup = BeautifulSoup(html, 'html.parser')
+    # Open the URL to read
+    html = urllib.request.urlopen(req).read()
+    # Parse the page with BeautifulSoup
+    soup = BeautifulSoup(html, 'html.parser')
         
-        # Souping elements according to the chosen software. Some work the same way so they have
-        # the same souping method.
-        if website == "1":
-            tags = soup('div', {'class': 'product-info'})
-            tagSoups.append(tags)
-        elif website == "2" or website == "4" or website == "5":
-            tags = soup('h2', {'class': 'entry-title'})
-            tagSoups.append(tags)
-        elif website == "3":
-            tags = soup('h1', {'class': 'entry-title'})
-            tagSoups.append(tags)
-        elif website == "6":
-            tags = soup('a')
-            tagSoups.append(tags)
-        elif website == "7":
-            tags = soup('a', {'class': 'block'})
-            tagSoups.append(tags)
-        elif website == "8":
-            tags = soup('div', {'class': 'thumb-content'})
-            tagSoups.append(tags)
+    # Souping elements according to the chosen software. Some work the same way so they have
+    # the same souping method.
+    # If the siteKey2-3 aren't 'null', then use the default method (specific class)
+    if siteKey2 != "null" and siteKey3 != "null":
+        tags = soup(siteKey1, {siteKey2: siteKey3})
+    # If it is null, then this is done on purpose as the websites have special conditions, for example
+    # 1337x and gog-games. Explained further afterwards.
+    else:
+        # soups without a specific class, the siteKey1 is most probably "a".
+        tags = soup(siteKey1)
         
-        # For each tag in tagSoups
-        for tagS in tagSoups:
-            # for each tag WITHIN the tagS (tags from tagSoups)
-            for tag in tagS:
-                # Find the "a" / links
-                links = tag.find('a')
+    # For each tag in tags...
+    for tag in tags:
+        # Find the "a" / links
+        links = tag.find('a')
 
-                # If it didn't result "None"...
-                if links != None:
-                    # Get the href (link) 
-                    mainLinkA = links.get("href")
-                    # If it isn't None...
-                    if mainLinkA != None:
-                        # If it doesn't start with the following links... (to prevent unneccessary links)
-                        if not mainLinkA.startswith("https://w14.monkrus.ws/search/label/") or not mainLinkA.startswith("https://w14.monkrus.ws/search?") or mainLinkA != "https://w14.monkrus.ws/":
-                            # If the website chosen is 8, append it with the link. Some sites don't include the primary URL within 
-                            # their href, so the program adds it.
-                            if website == "8":
-                                generalLinks.append("https://steamrip/" + mainLinkA)
-                            # If not, just append as it is. This is the default for most.
-                            else: 
-                                generalLinks.append(mainLinkA)
+        # If it didn't result "None"...
+        if links != None:
+            # Get the href (link) 
+            mainLinkA = links.get("href")
+            # If it isn't None...
+            if mainLinkA != None:
+                # If it doesn't start with the following links... (to prevent unneccessary links)
+                if not mainLinkA.startswith("https://w14.monkrus.ws/search/label/") or not mainLinkA.startswith("https://w14.monkrus.ws/search?") or mainLinkA != "https://w14.monkrus.ws/":
+                    # If the website chosen is 8, append it with the link. Some sites don't include the primary URL within 
+                    # their href, so the program adds it. This is a special conditon for site 8.
+                    if site_id == "8":
+                        resultLinks.append(siteLink + mainLinkA)
+                    # If not, just append as it is. This is the default for most.
+                    else: 
+                        resultLinks.append(mainLinkA)
                     
-                # If Links resulted None, then the following have special conditions. 
-                elif links == None:
-                    # Get the href from the tag, because at first we grabbed the "a" directly.
-                    mainLinkB = tag.get("href")
-                    # If it isn't None...
-                    if mainLinkB != None:
-                        # If it starts with /torrent and the url is 1337x, then append with the URL at first.
-                        # similar to the steamrip one above. Will implement a better, automatic method for this.
-                        if mainLinkB.startswith("/torrent") and url.startswith("https://1337x"):
-                            generalLinks.append("https://1337x" + mainLinkB)          
-                        # If it starts with /game and the url is gog-games, then append with the URL at first.              
-                        elif mainLinkB.startswith("/game") and url.startswith("https://gog-games.com"):
-                            generalLinks.append("https://gog-games.com" + mainLinkB)                     
+        # If Links resulted None, then the following have special conditions. 
+        elif links == None:
+            # Get the href from the tag, because at first we grabbed the "a" directly.
+            mainLinkB = tag.get("href")
+            # If it isn't None...
+            if mainLinkB != None:
+                # If it starts with /torrent and the url is 1337x, then append with the URL at first.
+                # similar to the steamrip one above.
+                # This is to prevent others being shown and the site's structure doesn't include the
+                # primary URL in the beginning, so we add it first.
+                if mainLinkB.startswith("/torrent") and siteLink.startswith("https://1337x"):
+                    resultLinks.append(siteLink + mainLinkB)          
+                # If it starts with /game and the url is gog-games, then append with the URL at first.              
+                elif mainLinkB.startswith("/game") and siteLink.startswith("https://gog-games.com"):
+                    resultLinks.append(siteLink + mainLinkB)                     
 
-        # If the length of generalLinks is greater than 0...
-        if len(generalLinks) > 0:
-            # Append the link to allLinks list.
-            for link in generalLinks:
-                allLinks.append(link)
+    # If the length of resultLinks is greater than 0...
+    if len(resultLinks) > 0:
+        # Append the link to allLinks list.
+        for link in resultLinks:
+            allLinks.append(link)
             
-            # append the first link to the bestResults list.
-            bestResults.append(generalLinks[0])
-        # Print that the URL showed no results.
-        else:
-            print("No results -", searchUrl)
+        # append the first link to the bestResults list.
+        bestResults.append(resultLinks[0])
+    # If it's less than 0, most probably 0 itself, print that the URL showed no results.
+    else:
+        print("No results -", searchUrl)
 
 
 # Checks the chosen number the user input and operates accordingly.
 def checkChosenNum(chosenNum,nameInput):
+    # If user chose 0 or empty...
     if chosenNum == '0' or chosenNum == '':
-        generalMethod("1",nameInput) # FileCR
-        generalMethod("2",nameInput) # monkrus
-        generalMethod("3",nameInput) # Fitgirl Repacks
-        generalMethod("4",nameInput) # FTUApps
-        generalMethod("5",nameInput) # VSTorrent
-        generalMethod("6",nameInput) # 1337x
-        generalMethod("7",nameInput) # GOG Games
-        generalMethod("8",nameInput) # STEAMRIP
+        # for each site in sites
+        for site in sites:
+            # Activate the generalMethod function with forwarding the site's ID.
+            generalMethod(nameInput, site["id"])
     
+    # If user chose -1...
     elif chosenNum == "-1":
-        generalMethod("1",nameInput) # FileCR
-        generalMethod("2",nameInput) # monkrus
-        generalMethod("4",nameInput) # FTUApps
-        generalMethod("5",nameInput) # VSTorrent
-        generalMethod("6",nameInput) # 1337x  
+        # for each site in sites
+        for site in sites:
+            # if the site's type is software OR duo (duo means the site results both software
+            # and games)...
+            if site["type"] == "software" or site["type"] == "duo":      
+                # Run generalMethod with forwarding its site["id"]  
+                generalMethod(nameInput, site["id"])
 
+    # If user chose -2... 
     elif chosenNum == "-2":
-        generalMethod("3",nameInput) # Fitgirl Repacks
-        generalMethod("6",nameInput) # 1337x
-        generalMethod("7",nameInput) # GOG Games
-        generalMethod("8",nameInput) # STEAMRIP
+        # for each site in sites
+        for site in sites:
+            # If the site's type is game or duo...
+            if site["type"] == "game" or site["type"] == "duo":     
+                # Run generalMethod with forwarding its site["id"]     
+                generalMethod(nameInput, site["id"])
 
+    # This runs as the default method, which is when the user types a specific number
+    # for a specific software.
     else:
-        generalMethod(chosenNum,nameInput)    
+        site_chosen = sites[int(chosenNum)-1]
+        generalMethod(nameInput, site_chosen["id"])    
     
     # At the end, it prints the results if the length of allLinks is greater than 0
     if len(allLinks) > 0:
@@ -197,8 +189,8 @@ def checkChosenNum(chosenNum,nameInput):
 def askUser():
     # Shows available options to search from
     print("Available websites:")
-    for webnum in website_library:
-        print("[", webnum, "] ", website_library[webnum])
+    for site in sites:
+        print("[" + site['id'] + "] " + site['name'])
     print("[0/empty] All Sites")
     print("[-1] All Software Sites")
     print("[-2] All Game Sites")
@@ -206,8 +198,10 @@ def askUser():
 
     chosenNum = input("Where do you want to search? Enter number: ")
 
+    # If the user didn't type exit, ask the software's name. You can type "exit" to quit the program
     if chosenNum != 'exit':
         nameInput = input("Enter Software Name - ")
+        # If the user didn't type exit, run checkChosenNum.
         if nameInput != 'exit':
             checkChosenNum(chosenNum,nameInput)
         else:
@@ -217,5 +211,6 @@ def askUser():
         print("Exiting Program...")
         exit()
 
+# Welcome message, beginning of program.
 print("\n ---> Welcome to pSearch - Python based Piracy searching tool <--- \n")
 askUser()
