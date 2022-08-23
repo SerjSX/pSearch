5c8cf347dfb94f3c4486fdfa790adc667749d0e9
Serj <74871847+SerjSX@users.noreply.github.com>
2022-08-23 12:11:07 +0300

    Fixed rep results and JSON update
    Fixed bug where if you search again old results showed up as well.
    
    Added "mainlink" to the JSON section in order to identify if the link grabbed will start with the site's link or not, if not then the program adds it accordingly.

--- pSearch.py
+++ pSearch.py
@@ -6,13 +6,14 @@ import json
 sites_json = ''' [
     {
         "id": "1",
-        "name": "FileCR - All",
+        "name": "FileCR",
         "link": "https://filecr.com/",
         "slink": "https://filecr.com/?s=",
         "skey1": "div",
         "skey2": "class",
         "skey3": "product-info",
-        "type": ["software", "android"]
+        "type": ["software", "android"],
+        "mainlink": "yes"
     },
     {
         "id": "2",
@@ -22,7 +23,8 @@ sites_json = ''' [
         "skey1": "h2",
         "skey2": "class",
         "skey3": "entry-title",
-        "type": ["software"]
+        "type": ["software"],
+        "mainlink": "yes"
     },
     {
         "id": "3",
@@ -32,7 +34,8 @@ sites_json = ''' [
         "skey1": "h1",
         "skey2": "class",
         "skey3": "entry-title",
-        "type": ["game"]
+        "type": ["game"],
+        "mainlink": "yes"
     },
     {
         "id": "4",
@@ -42,7 +45,8 @@ sites_json = ''' [
         "skey1": "h2",
         "skey2": "class",
         "skey3": "entry-title",
-        "type": ["software"]
+        "type": ["software"],
+        "mainlink": "yes"
     },
     {
         "id": "5",
@@ -52,7 +56,8 @@ sites_json = ''' [
         "skey1": "h2",
         "skey2": "class",
         "skey3": "entry-title",
-        "type": ["software"]
+        "type": ["software"], 
+        "mainlink": "yes"
     },    
     {
         "id": "6",
@@ -62,7 +67,8 @@ sites_json = ''' [
         "skey1": "a",
         "skey2": "null",
         "skey3": "null",
-        "type": ["software", "game", "android", "movie"]
+        "type": ["software", "game", "android", "movie"],
+        "mainlink": "no"
     },    
     {
         "id": "7",
@@ -72,7 +78,8 @@ sites_json = ''' [
         "skey1": "a",
         "skey2": "class",
         "skey3": "block",
-        "type": ["game"]
+        "type": ["game"],
+        "mainlink": "no"
     },    
     {
         "id": "8",
@@ -82,7 +89,8 @@ sites_json = ''' [
         "skey1": "div",
         "skey2": "class",
         "skey3": "thumb-content",
-        "type": ["game"]
+        "type": ["game"],
+        "mainlink": "yes"
     },
     {
         "id": "9",
@@ -92,7 +100,8 @@ sites_json = ''' [
         "skey1": "h2",
         "skey2": "null",
         "skey3": "null",
-        "type": ["software"]
+        "type": ["software"],
+        "mainlink": "yes"
     },
     {
         "id": "10",
@@ -102,7 +111,8 @@ sites_json = ''' [
         "skey1": "h3",
         "skey2": "class",
         "skey3": "post-title entry-title",
-        "type": ["android"]        
+        "type": ["android"],
+        "mainlink": "yes"   
     },
     {
         "id": "11",
@@ -112,7 +122,8 @@ sites_json = ''' [
         "skey1": "div",
         "skey2": "class",
         "skey3": "bloque-app",
-        "type": ["android"]
+        "type": ["android"],
+        "mainlink": "yes"
     }
 ]'''
 
@@ -135,7 +146,7 @@ bestResults = list()
 allLinks = list()
 
 # generalMethod(nameInput,site_id) is the main function that searching process works in
-def generalMethod(nameInput, site_id, chosen_type):
+def generalMethod(nameInput, site_id, chosen_type, main_link):
     # These variables below are for the information necessary for the searching process.
     # siteLink has the normal link of the website, useful when the grabbed URL doesn't start with the
     # website URL, so we can just add it easily.
@@ -165,7 +176,7 @@ def generalMethod(nameInput, site_id, chosen_type):
     # If it starts with https://1337x.to/ then quote it with %20 (space), this is unique for this site 
     # because of its web structure.
     if siteLink.startswith("https://1337x.to/"):
-        # If chosen_type is Android, add the keyword "android" at the end.
+        # If chosen_type is Android, add the keyword "android" at the end for accurate results.
         if chosen_type == "android":
             nameInputFixed = urllib.parse.quote(nameInput + " android")
             # Connect the url with the software name the user put at the beginning
@@ -174,10 +185,12 @@ def generalMethod(nameInput, site_id, chosen_type):
             nameInputFixed = urllib.parse.quote(nameInput)
             # Connect the url with the software name the user put at the beginning
             searchUrl = siteSLink + nameInputFixed + "/1/"
-    # If it doesn't starts with https://1337x.to/ then quote it with + (it replaces space with +), this is
-    # the default method as most sites work this way.
+    # If the site is FileCR and the chosen type is Android, then specifically search it in 
+    # the android section for accurate results.
     elif site_id == "1" and chosen_type == "android":
         searchUrl = siteSLink + urllib.parse.quote(nameInput) + "&subcat_filter=&category-type=23"
+    # If it doesn't starts with https://1337x.to/ then quote it with + (it replaces space with +), this is
+    # the default method as most sites work this way.
     else:      
         nameInputFixed = urllib.parse.quote_plus(nameInput)
         # Connect the url with the software name the user put at the beginning
@@ -189,8 +202,9 @@ def generalMethod(nameInput, site_id, chosen_type):
     # Send a request to connect to the site with the header.
     req = urllib.request.Request(url=searchUrl, headers=header) 
 
-    # Open the URL to read
+    # Open the URL to read        
     html = urllib.request.urlopen(req).read()
+    
     # Parse the page with BeautifulSoup
     soup = BeautifulSoup(html, 'html.parser')
         
@@ -220,7 +234,7 @@ def generalMethod(nameInput, site_id, chosen_type):
                 if not mainLinkA.startswith("https://w14.monkrus.ws/search/label/") or not mainLinkA.startswith("https://w14.monkrus.ws/search?") or mainLinkA != "https://w14.monkrus.ws/":
                     # If the website chosen is 8, append it with the link. Some sites don't include the primary URL within 
                     # their href, so the program adds it. This is a special conditon for site 8.
-                    if site_id == "8":
+                    if main_link == "no":
                         resultLinks.append(siteLink + mainLinkA)
                     # If not, just append as it is. This is the default for most.
                     else: 
@@ -236,7 +250,7 @@ def generalMethod(nameInput, site_id, chosen_type):
                 # similar to the steamrip one above.
                 # This is to prevent others being shown and the site's structure doesn't include the
                 # primary URL in the beginning, so we add it first.
-                if mainLinkB.startswith("/torrent") and siteLink.startswith("https://1337x"):
+                if mainLinkB.startswith("/torrent"):
                     resultLinks.append(siteLink + mainLinkB)          
                 # If it starts with /game and the url is gog-games, then append with the URL at first.              
                 elif mainLinkB.startswith("/game") and siteLink.startswith("https://gog-games.com"):
@@ -262,7 +276,7 @@ def checkChosenNum(chosenNum,nameInput):
         # for each site in sites
         for site in sites:
             # Activate the generalMethod function with forwarding the site's ID.
-            generalMethod(nameInput, site["id"], 0)
+            generalMethod(nameInput, site["id"], 0, site["mainlink"])
     
     # If user chose -1...
     elif chosenNum == "-1":
@@ -272,7 +286,7 @@ def checkChosenNum(chosenNum,nameInput):
             # and games)...
             if "software" in site["type"]:      
                 # Run generalMethod with forwarding its site["id"]  
-                generalMethod(nameInput, site["id"], 0)
+                generalMethod(nameInput, site["id"], 0, site["mainlink"])
 
     # If user chose -2... 
     elif chosenNum == "-2":
@@ -281,23 +295,23 @@ def checkChosenNum(chosenNum,nameInput):
             # If the site's type is game or duo...
             if "game" in site["type"]:     
                 # Run generalMethod with forwarding its site["id"]     
-                generalMethod(nameInput, site["id"], 0)
+                generalMethod(nameInput, site["id"], 0, site["mainlink"])
 
     elif chosenNum == "-3":
         for site in sites:
             if "android" in site["type"]:
-                generalMethod(nameInput, site["id"], "android")
+                generalMethod(nameInput, site["id"], "android", site["mainlink"])
             
     elif chosenNum == "-4":
         for site in sites:
             if "movie" in site["type"]:
-                generalMethod(nameInput, site["id"], 0)
+                generalMethod(nameInput, site["id"], 0, site["mainlink"])
 
     # This runs as the default method, which is when the user types a specific number
     # for a specific software.
     else:
         site_chosen = sites[int(chosenNum)-1]
-        generalMethod(nameInput, site_chosen["id"], 0)    
+        generalMethod(nameInput, site_chosen["id"], 0, site_chosen["mainlink"])    
     
     # At the end, it prints the results if the length of allLinks is greater than 0
     if len(allLinks) > 0:
@@ -315,7 +329,9 @@ def checkChosenNum(chosenNum,nameInput):
         print("\nNo results")
 
     searchAsk = input("\nSearch again? [Y/n]")
-    if searchAsk == "" or searchAsk == "Y" or searchAsk == "y":
+    if searchAsk == "" or searchAsk == "Y" or searchAsk == "y":  
+        allLinks.clear()
+        bestResults.clear()
         askUser()
     else: 
         print("Quitting program...")
@@ -327,12 +343,12 @@ def askUser():
     print("Available websites:")
     for site in sites:
         print("[" + site['id'] + "] " + site['name'])
-    print("[0/empty] All Sites")
+    print("\n[0/empty] All Sites")
     print("[-1] All Software Sites")
     print("[-2] All Game Sites")
     print("[-3] All Android Sites")
     print("[-4] All Movie/Series Sites")
-    print("[exit/quit] Quits the program")
+    print("[exit] Quits the program\n")
 
     chosenNum = input("Where do you want to search? Enter number: ")
 
